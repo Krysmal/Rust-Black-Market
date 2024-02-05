65e712f6cebf5d2b6ef28001588efa5127cf6d3e
Krzysztof Malinowski <malinowskizxcvbnm@gmail.com>
2024-02-05 11:47:15 +0100

    start

---
+++ src/Components/CHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CHeader.css";
+function CHeader() {
+    return (
+      <div className='header'>
+  
+          
+      </div>
+    );
+  }
+  export default CHeader;
