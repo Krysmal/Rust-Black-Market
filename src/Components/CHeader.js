23ed55f2506e0a468880240870aef52fa4d68b99
markussaila <makexxc@gmail.com>
2024-02-05 13:32:32 +0200

    header progress 1

--- src/Components/CHeader.js
+++ src/Components/CHeader.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./CHeader.css";
 function CHeader() {
     return (
+
+      
       <div className='header'>
-  
+        <h1>Community goods and services</h1>
           
       </div>
     );
