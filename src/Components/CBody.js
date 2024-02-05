c1096d24165de583af08676ae6ee8e47b75795ba
Krzysztof Malinowski <malinowskizxcvbnm@gmail.com>
2024-02-05 12:09:07 +0100

    body

---
+++ src/Components/CBody.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CBody.css";
+function CBody() {
+    return (
+      <div className='body'>
+  
+          
+      </div>
+    );
+  }
+  export default CBody;
